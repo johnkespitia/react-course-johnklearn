e9d298f380835c564815690db1086f35affc5ab6
johnk_ <john.espitia@dafiti.com.co>
2020-01-28 15:02:36 -0500

    Inclusión de eventos y reactividad con state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './styles/Navbar.css'
 class Navbar extends React.Component{
     render(){
         return (
-            <div class="Navbar">
+            <div className="Navbar">
                 <div className="container-fluid">
                     <a href="/" className="Navbar__brand" >
                         <img className="Navbar__brand-logo" src={logo} alt="" />
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,7 +3,32 @@ import './styles/BadgeNew.css'
 import Navbar from '../components/Navbar'
 import header from '../images/badge.header.jpg'
 import Badge from '../components/Badge'
+import BadgeForm from '../components/BadgeForm'
 class BadgeNew extends React.Component{
+    state = { 
+        form: {
+            jobTitle: 'Designer',
+            firstname:'JohnK',
+            lastName:'',
+            email:'',
+            twitter:''
+        } 
+    };
+
+    handleChange = e => {
+        //const nextForm = this.state.form;
+        //nextForm[e.target.name]= e.target.value;
+        //this.setState({
+        //    form: nextForm
+        //})
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     render(){
         return (
             <div>
@@ -15,11 +40,15 @@ class BadgeNew extends React.Component{
                     <div className="row">
                         <div className="col">
                             <Badge 
-                                firstName="JohnK"
-                                lastName="Espitia"
-                                twitter="johnkespitia"
-                                jobTitle="FullStack Developer"
+                                firstName={this.state.form.firstname}
+                                lastName={this.state.form.lastName}//"Espitia"
+                                twitter={this.state.form.twitter}//"johnkespitia"
+                                jobTitle={this.state.form.jobTitle}//"FullStack Developer"
+                                email={this.state.form.email}//"FullStack Developer"
                                 avatarUrl="https://www.gravatar.com/avatar/0628601f7d228fda7a1bc239335ce680?d=identicon" />
+                        </div>
+                        <div className="col">
+                            <BadgeForm onChange={this.handleChange} formValues={this.state.form} />
                         </div>
                     </div>
                 </div>
